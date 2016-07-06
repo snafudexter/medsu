cb877c49e97bb292b7ac987a259b4ea15737bb65
snafudexter <prabh@live.in>
2016-07-06 14:46:42 +0530

    save

--- app/src/main/java/net/medsu/android/medsu/LoginActivity.java
+++ app/src/main/java/net/medsu/android/medsu/LoginActivity.java
@@ -2,12 +2,45 @@ package net.medsu.android.medsu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.HashMap;
 
+public class LoginActivity extends AppCompatActivity implements Button.OnClickListener, RestHelper.RestListner{
+
+    RestHelper restHelper;
+
+    EditText mail, pass;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mail = (EditText) findViewById(R.id.txtMail);
+        pass = (EditText) findViewById(R.id.txtPassword);
+        login = (Button) findViewById(R.id.btnLogin);
+
+        //prepare for login on click
+        HashMap hashMap = new HashMap();
+        hashMap.put("mail", mail.getText());
+        hashMap.put("pass", pass.getText());
+        restHelper = new RestHelper(getString(R.string.domain)+getString(R.string.login),hashMap);
+        restHelper.setRestListner(this);
+
     }
+
+    @Override
+    public void callback(String json)
+    {
+        //prepare to get token
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
--- app/src/main/java/net/medsu/android/medsu/RestHelper.java
+++ app/src/main/java/net/medsu/android/medsu/RestHelper.java
@@ -1,7 +1,105 @@
 package net.medsu.android.medsu;
 
+import android.net.http.HttpResponseCache;
+import android.os.AsyncTask;
+import android.provider.Settings;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by prabhjot on 6/17/2016.
  */
-public class RestHelper {
+public class RestHelper extends AsyncTask<Void, Void, String>
+{
+    public interface RestListner{
+        public void callback(String json);
+    }
+
+    void setRestListner(RestListner r)
+    {
+        restListner = r;
+    }
+    RestListner restListner;
+
+
+    String url;
+    HttpClient client;
+    HttpPost post;
+    List<NameValuePair> lParams;
+    public RestHelper(String url, HashMap params)
+    {
+        this.url = url;
+        lParams = new ArrayList<>();
+        Set set = params.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext())
+        {
+            Map.Entry me = (Map.Entry)iterator.next();
+            lParams.add(new BasicNameValuePair(me.getKey().toString(), me.getValue().toString()));
+        }
+        client = new DefaultHttpClient();
+        post = new HttpPost(url);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        restListner.callback(s);
+    }
+
+    @Override
+    protected String doInBackground(Void... params)
+    {
+        try {
+
+            post.setEntity(new UrlEncodedFormEntity(lParams));
+            post.addHeader("Content-Type", "x-www-form-urlencoded");
+            HttpResponse response = client.execute(post);
+
+            int responseCode = response.getStatusLine().getStatusCode();
+            if(responseCode == 200)
+            {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append("\n");
+                }
+
+                return stringBuilder.toString();
+            }
+            else
+            {
+                throw new Exception("ResponseCode: " + responseCode);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Log.e("medsuErr", "RestHelper" + e.getMessage());
+
+        }
+        return null;
+    }
 }
+
+
